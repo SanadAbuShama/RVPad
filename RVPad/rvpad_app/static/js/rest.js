cc251515a79faaf9839bf1ab8cdbe8ee5142d2da
Sanad Abu Shama <abushamasanad@hotmail.com>
2022-02-26 20:09:38 +0200

    Added jQuery instant validations for all forms

--- RVPad/rvpad_app/static/js/rest.js
+++ RVPad/rvpad_app/static/js/rest.js
@@ -1,15 +1,44 @@
-$(document).ready(function(){
-    $('#name').keyup(function(){
-        var data = $("#restForm").serialize()   // capture all the data in the form in the variable data
-        $.ajax({
-            method: "POST",   // we are using a post request here, but this could also be done with a get
-            url: "/rvpad/name",
-            data: data
-        })
-        .done(function(res){
-             $('#nameMsg').html(res)  // manipulate the dom when the response comes back
-             $('#nameMsg,.success').css('color','#54f542')
-             $('#nmaeMsg,.error').css('color','#f5ed05')
-        })
-    })
-})
+$(document).ready(function () {
+  $("#name").keyup(function () {
+    var data = $("#restForm").serialize(); // capture all the data in the form in the variable data
+    $.ajax({
+      method: "POST", // we are using a post request here, but this could also be done with a get
+      url: "/rvpad/name",
+      data: data,
+    }).done(function (res) {
+      $("#nameMsg").html(res); // manipulate the dom when the response comes back
+      $("#nameMsg,.success").css("color", "#54f542");
+      $("#nmaeMsg,.error").css("color", "#f5ed05");
+    });
+  });
+
+  //Location
+
+  $("#location").keyup(function () {
+    value = $("#location").val();
+    if (value.length < 2) {
+      $("#location_msg").html(
+        "<p>Location should be at least 2 characters</p>"
+      );
+      $("#location_msg").css("color", "#f5ed05");
+    } else {
+      $("#location_msg").html("<p>Looks Good!</p>");
+      $("#location_msg").css("color", "#54f542");
+    }
+  });
+
+  //Description
+
+  $("#desc").keyup(function () {
+    value = $("#desc").val();
+    if (value.length < 10) {
+      $("#desc_msg").html(
+        "<p>Description should be at least 10 characters</p>"
+      );
+      $("#desc_msg").css("color", "#f5ed05");
+    } else {
+      $("#desc_msg").html("<p>Looks Good!</p>");
+      $("#desc_msg").css("color", "#54f542");
+    }
+  });
+});
--- RVPad/rvpad_app/static/js/user.js
+++ RVPad/rvpad_app/static/js/user.js
@@ -1,15 +1,58 @@
-$(document).ready(function(){
-    $('#email').keyup(function(){
-        var data = $("#regForm").serialize()   // capture all the data in the form in the variable data
-        $.ajax({
-            method: "POST",   // we are using a post request here, but this could also be done with a get
-            url: "/email",
-            data: data
-        })
-        .done(function(res){
-             $('#emailMsg').html(res)  // manipulate the dom when the response comes back
-             $('#emailMsg,.success').css('color','#54f542')
-             $('#emailMsg,.error').css('color','#f5ed05')
-        })
-    })
-})
+$(document).ready(function () {
+  $("#email").keyup(function () {
+    var data = $("#regForm").serialize(); // capture all the data in the form in the variable data
+    $.ajax({
+      method: "POST", // we are using a post request here, but this could also be done with a get
+      url: "/email",
+      data: data,
+    }).done(function (res) {
+      $("#emailMsg").html(res); // manipulate the dom when the response comes back
+      $("#emailMsg,.success").css("color", "#54f542");
+      $("#emailMsg,.error").css("color", "#f5ed05");
+    });
+  });
+
+  // First Name
+  $("#first_name").keyup(function () {
+    value = $("#first_name").val();
+    if (value.length < 2) {
+      $("#first_name_msg").html(
+        "<p>First name should be at least 2 characters</p>"
+      );
+      $("#first_name_msg").css("color", "#f5ed05");
+    } else {
+      $("#first_name_msg").html("<p>Looks Good!</p>");
+      $("#first_name_msg").css("color", "#54f542");
+    }
+  });
+
+  //Last name
+
+  $("#last_name").keyup(function () {
+    value = $("#last_name").val();
+    if (value.length < 2) {
+      $("#last_name_msg").html(
+        "<p>Last name should be at least 2 characters</p>"
+      );
+      $("#last_name_msg").css("color", "#f5ed05");
+    } else {
+      $("#last_name_msg").html("<p>Looks Good!</p>");
+      $("#last_name_msg").css("color", "#54f542");
+    }
+  });
+
+  //Password
+
+  $("#password").keyup(function () {
+    value = $("#password").val();
+    if (value.length < 8) {
+      $("#password_msg").html(
+        "<p>Password should be at least 8 characters</p>"
+      );
+      $("#password_msg").css("color", "#f5ed05");
+    } else {
+      $("#password_msg").html("<p>Looks Good!</p>");
+      $("#password_msg").css("color", "#54f542");
+    }
+  });
+});
